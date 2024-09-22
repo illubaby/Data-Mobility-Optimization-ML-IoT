f6821e98558ce44b8b211b067464bacc1a353df9
illubaby <137404278+illubaby@users.noreply.github.com>
2024-09-22 23:32:40 +0700

    Synthesys and run Test 2 block design

--- Hardware/Vivado/Data_Mobility/Data_Mobility.runs/impl_1/runme.sh
+++ Hardware/Vivado/Data_Mobility/Data_Mobility.runs/impl_1/runme.sh
@@ -25,7 +25,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/ADMIN/OneDrive/Documents/GitHub/Data-Mobility-Optimization-ML-IoT/Hardware/Vivado/Data_Mobility/Data_Mobility.runs/impl_1'
+HD_PWD='C:/Data-Mobility-Optimization-ML-IoT/Hardware/Vivado/Data_Mobility/Data_Mobility.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
@@ -42,7 +42,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log Test_2_wrapper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source Test_2_wrapper.tcl -notrace
 
 
